57e4739fb24eb088895eeba60ff4fcbb0f3910b0
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2024-01-31 21:31:53 +0100

    chore: update display drivers

--- src/main.cpp
+++ src/main.cpp
@@ -9,38 +9,24 @@
 #include "Utils.h"
 #include "GameSwitcher.h"
 
-static lv_disp_draw_buf_t draw_buf;
+static lv_display_t * display;
 M5Display *tft;
 
-void tft_lv_initialization() {
-  M5.begin(true, false);
-
-  lv_init();
-
-  static lv_color_t buf1[(LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10];  // Declare a buffer for 1/10 screen siz
-  static lv_color_t buf2[(LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10];  // second buffer is optionnal
-
-  // Initialize `disp_buf` display buffer with the buffer(s).
-  lv_disp_draw_buf_init(&draw_buf, buf1, buf2, (LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10);
-
-  tft = &M5.Lcd;
-}
-
 // Display flushing
-void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
+void my_display_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map) {
   uint32_t w = (area->x2 - area->x1 + 1);
   uint32_t h = (area->y2 - area->y1 + 1);
 
   tft->startWrite();
   tft->setAddrWindow(area->x1, area->y1, w, h);
-  tft->pushColors((uint16_t *)&color_p->full, w * h, true);
+  tft->pushColors((uint16_t *)px_map, w * h, true);
   tft->endWrite();
 
-  lv_disp_flush_ready(disp);
+  lv_display_flush_ready(disp);
 }
 
 // Read the touchpad
-void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
+void my_touchpad_read(lv_indev_t *indev, lv_indev_data_t *data) {
   TouchPoint_t pos = M5.Touch.getPressPoint();
 
   bool touched = (pos.x <= 0 || pos.y <= 0 || pos.x >= LV_HOR_RES_MAX || pos.y >= LV_VER_RES_MAX) ? false : true;
@@ -55,38 +41,36 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
 
 // Initialize the touch screen
 void init_touch_driver() {
-  static lv_indev_drv_t indev_drv;
-
-  lv_indev_drv_init(&indev_drv);
-
-  indev_drv.type = LV_INDEV_TYPE_POINTER;
-  indev_drv.read_cb = my_touchpad_read;  // set touchpad callback
-
-  lv_indev_drv_register(&indev_drv);  // Register the driver
+  lv_indev_t *indev = lv_indev_create();
+  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+  lv_indev_set_read_cb(indev, my_touchpad_read);
+  lv_indev_set_display(indev, display);
 }
 
-// Initialize the display
-void init_disp_driver() {
-  static lv_disp_drv_t disp_drv;  // Descriptor of a display driver
+void custom_log_cb(lv_log_level_t level, const char *buf) { Serial.println(buf); }
 
-  lv_disp_drv_init(&disp_drv);  // Basic initialization
+void tft_lv_initialization() {
+  M5.begin(true, false);
 
-  disp_drv.flush_cb = my_disp_flush;  // Set your driver function
-  disp_drv.draw_buf = &draw_buf;      // Assign the buffer to the display
-  disp_drv.hor_res = LV_HOR_RES_MAX;  // Set the horizontal resolution of the display
-  disp_drv.ver_res = LV_VER_RES_MAX;  // Set the vertical resolution of the display
+  lv_init();
 
-  lv_disp_drv_register(&disp_drv);  // Finally register the driver
+  static uint16_t buf1[(LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10];  // Declare a buffer for 1/10 screen siz
+  static uint16_t buf2[(LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10];  // second buffer is optionnal
+
+  lv_display_t * display = lv_display_create(LV_HOR_RES_MAX, LV_VER_RES_MAX);
+
+  // Initialize `display_buf` display buffer with the buffer(s).
+  lv_display_set_buffers(display, buf1, buf2, sizeof(buf1), LV_DISPLAY_RENDER_MODE_FULL);
+  lv_display_set_flush_cb(display, my_display_flush);
+
+  tft = &M5.Lcd;
 }
-
-void custom_log_cb(const char *buf) { Serial.println(buf); }
 
 void setup() {
   randomSeed(analogRead(ANALOG_PIN));
 
   tft_lv_initialization();
 
-  init_disp_driver();
   init_touch_driver();
 
   lv_port_littlefs_init();
