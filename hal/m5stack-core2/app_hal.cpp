38525e836605acf02416d512ec61156b060a9f72
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2024-02-20 20:52:49 +0100

    test: remove useless file for testing littlefs lfs.h implementation

--- hal/m5stack-core2/app_hal.cpp
+++ hal/m5stack-core2/app_hal.cpp
@@ -2,8 +2,10 @@
 #include "lvgl.h"
 #include "Arduino.h"
 #include "M5Core2.h"
-#include "drivers/lv_fs_littlefs.h"
 #include "app_hal.h"
+#include "lfs.h"
+#include "Utils.h"
+#include "esp_partition.h"
 
 M5Display *tft;
 
@@ -59,12 +61,115 @@ void init_touch_driver(void) {
   lv_indev_set_read_cb(indev, touchpad_read_cb);
 }
 
+static uint32_t s_blockSize = 4096;
+
+int lfs_flash_read(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size) {
+  // Calculate the address to read from
+    size_t addr = c->block_size * block + off;
+
+    // Find the data partition
+    const esp_partition_t *partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_SPIFFS, NULL);
+    if (partition == NULL) {
+        return LFS_ERR_IO;
+    }
+
+    // Read data from the flash memory
+    esp_err_t err = esp_partition_read(partition, addr, buffer, size);
+    if (err != ESP_OK) {
+        return LFS_ERR_IO;
+    }
+
+    return 0;
+}
+
+int lfs_flash_prog(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size) {
+    const esp_partition_t *partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_SPIFFS, NULL);
+    if (partition == NULL) {
+        return LFS_ERR_IO;
+    }
+
+    const esp_partition_t *next_partition = NULL;
+    size_t num_partitions;
+    esp_partition_iterator_t it = esp_partition_find(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_SPIFFS, NULL);
+
+    // Iterate over all partitions
+    while (it != NULL) {
+        const esp_partition_t *part = esp_partition_get(it);
+        if (part->address > partition->address) {
+            next_partition = part;
+            break;
+        }
+        it = esp_partition_next(it);
+    }
+
+    // If next_partition is still NULL, it means there are no partitions after the current one
+    if (next_partition == NULL) {
+        return LFS_ERR_IO;
+    }
+
+    // Calculate the size of the current partition
+    size_t partition_size = next_partition->address - partition->address;
+
+    size_t addr = partition->address + (block * c->block_size) + off;
+
+    if (addr + size > partition_size) {
+        return LFS_ERR_IO;
+    }
+
+    return esp_partition_write(partition, addr, buffer, size);
+}
+
+
+int lfs_flash_erase(const struct lfs_config *c, lfs_block_t block) { return 0; }
+
+int lfs_flash_sync(const struct lfs_config *c) {
+  (void)c;
+  return 0;
+}
+
 // Initialize hal
 void hal_setup(void) {
   randomSeed(analogRead(ANALOG_PIN));
   M5.begin(true, false);
 
   lv_fs_littlefs_init();
+#define LFS_CACHE_SIZE 512U
+
+uint8_t readBuff[LFS_CACHE_SIZE];
+uint8_t writeBuff[LFS_CACHE_SIZE];
+uint8_t lookBuff[LFS_CACHE_SIZE];
+uint8_t fileBuff[LFS_CACHE_SIZE];
+
+  static lfs_t s_fs;
+  static lfs_config s_cfg;
+  memset(&s_fs, 0, sizeof(s_fs));
+  memset(&s_cfg, 0, sizeof(s_cfg));
+  s_cfg.read = lfs_flash_read;
+  s_cfg.prog = lfs_flash_prog;
+  s_cfg.erase = lfs_flash_erase;
+  s_cfg.sync = lfs_flash_sync;
+
+  s_cfg.read_size = 64;
+  s_cfg.prog_size = 64;
+  s_cfg.block_size = s_blockSize;
+  s_cfg.block_count = 3538944 / s_blockSize;
+  s_cfg.block_cycles = 16;  // TODO - need better explanation
+  s_cfg.cache_size = 64;
+  s_cfg.lookahead_size = 64;
+  s_cfg.read_buffer = nullptr;
+  s_cfg.prog_buffer = nullptr;
+  s_cfg.lookahead_buffer = nullptr;
+  s_cfg.name_max = 0;
+  s_cfg.file_max = 0;
+  s_cfg.attr_max = 0;
+
+  int res = lfs_mount(&s_fs, &s_cfg);
+  if (res) {
+    serial_printf("LITTLEFS", "not mounted");
+  } else {
+    serial_printf("LITTLEFS", "mounted");
+  }
+  lv_littlefs_set_handler(&s_fs);
   init_touch_driver();
 
   tft = &M5.Lcd;
--- hal/m5stack-core2/drivers/lv_fs_littlefs.h
+++ hal/m5stack-core2/drivers/lv_fs_littlefs.h
@@ -6,7 +6,9 @@ extern "C" {
 #endif
 
 #include "lvgl.h"
+#include "lfs.h"
 
+void lv_littlefs_set_handler(lfs_t* lfs);
 void lv_fs_littlefs_init(void);
 
 #ifdef __cplusplus
