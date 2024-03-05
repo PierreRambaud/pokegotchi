dfd1db7a56dae487f3fcc1bbb3cfda5093d7e865
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2024-03-05 21:20:35 +0100

    fix: force lv_imagebutton_set_src to use all parameters

--- lib/Pokegotchi/ActionsMenu.cpp
+++ lib/Pokegotchi/ActionsMenu.cpp
@@ -81,7 +81,7 @@ ActionsMenu::ActionsMenu(Menu* menu) {
   _items[14] = new BagItem{&object_water, _("bag.water.name"), _("bag.water.description"), new BagItemSpecifications{15, 0, 0, 0, 0, 4}};
 
   _bag_button = lv_menu_button_create(_menu->get_menu_screen(), &menu_bag, &menu_bag_pressed, _("actions.menu.bag"));
-  lv_imagebutton_set_src(_bag_button, LV_IMAGEBUTTON_STATE_DISABLED, NULL, &menu_bag_disabled, NULL);
+  lv_imagebutton_set_src(_bag_button, LV_IMAGEBUTTON_STATE_DISABLED, &menu_bag_disabled, &menu_bag_disabled, &menu_bag_disabled);
   lv_obj_add_event_cb(_bag_button, display_bag_items_event_handler, LV_EVENT_CLICKED, NULL);
 
   _sleep_button = lv_menu_button_create(_menu->get_menu_screen(), &menu_flute, &menu_flute_pressed, Pokemon::getInstance()->is_sleeping() ? _("actions.menu.wake_up") : _("actions.menu.sleep"));
@@ -89,11 +89,11 @@ ActionsMenu::ActionsMenu(Menu* menu) {
   lv_obj_add_event_cb(_sleep_button, toggle_sleep_event_handler, LV_EVENT_CLICKED, sleep_label);
 
   _train_button = lv_menu_button_create(_menu->get_menu_screen(), &menu_train, &menu_train_pressed, _("menu.train"));
-  lv_imagebutton_set_src(_train_button, LV_IMAGEBUTTON_STATE_DISABLED, NULL, &menu_train_disabled, NULL);
+  lv_imagebutton_set_src(_train_button, LV_IMAGEBUTTON_STATE_DISABLED, &menu_train_disabled, &menu_train_disabled, &menu_train_disabled);
   lv_obj_add_event_cb(_train_button, train_event_handler, LV_EVENT_CLICKED, NULL);
 
   _play_button = lv_menu_button_create(_menu->get_menu_screen(), &menu_play, &menu_play_pressed, _("menu.play"));
-  lv_imagebutton_set_src(_play_button, LV_IMAGEBUTTON_STATE_DISABLED, NULL, &menu_play_disabled, NULL);
+  lv_imagebutton_set_src(_play_button, LV_IMAGEBUTTON_STATE_DISABLED, &menu_play_disabled, &menu_play_disabled, &menu_play_disabled);
   lv_obj_add_event_cb(_play_button, play_event_handler, LV_EVENT_CLICKED, NULL);
 
   _clean_button = lv_menu_button_create(_menu->get_menu_screen(), &menu_clean, &menu_clean_pressed, _("actions.menu.clean"));
@@ -101,7 +101,7 @@ ActionsMenu::ActionsMenu(Menu* menu) {
 
   _heal_button = lv_menu_button_create(_menu->get_menu_screen(), &menu_heal, &menu_heal_pressed, _("actions.menu.heal"));
   set_heal_menu_text(lv_obj_get_child(_heal_button, -1));
-  lv_imagebutton_set_src(_heal_button, LV_IMAGEBUTTON_STATE_DISABLED, NULL, &menu_heal_disabled, NULL);
+  lv_imagebutton_set_src(_heal_button, LV_IMAGEBUTTON_STATE_DISABLED, &menu_heal_disabled, &menu_heal_disabled, &menu_heal_disabled);
   lv_obj_add_event_cb(_heal_button, heal_event_handler, LV_EVENT_CLICKED, NULL);
 
   serial_printf("ActionsMenu", "Buttons for actions menu created");
